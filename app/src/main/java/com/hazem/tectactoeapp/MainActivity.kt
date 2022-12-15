17a2ea693f2e950ce6c0ae7ba27117401133cb30
hazem khaled <hazemk778@gmail.com>
2022-12-15 23:29:46 +0200

    reSet and isWin functions is Created successfully

--- app/src/main/java/com/hazem/tectactoeapp/MainActivity.kt
+++ app/src/main/java/com/hazem/tectactoeapp/MainActivity.kt
@@ -8,35 +8,99 @@ import com.hazem.tectactoeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-     private var currPlayer:Boolean=true
-    private val array = Array(3) { IntArray(3) }
+    private var currPlayer: Boolean = true
+    private var array = Array(3) { IntArray(3) { 2 } }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
-        array[0][0]=1
-        binding.tv11.setOnClickListener {
-            play(currPlayer,binding.tv11,1,1)
-          //  Toast.makeText(applicationContext,"${array[0][0]}",Toast.LENGTH_LONG).show()
+
+       start()
+    }
+    private fun start(){
+    binding.tv11.setOnClickListener {
+        play(currPlayer, binding.tv11, 1, 1)
+    }
+    binding.tv12.setOnClickListener {
+        play(currPlayer, binding.tv12, 1, 2)
+    }
+    binding.tv13.setOnClickListener {
+        play(currPlayer, binding.tv13, 1, 3)
+    }
+    binding.tv21.setOnClickListener {
+        play(currPlayer, binding.tv21, 2, 1)
+    }
+    binding.tv22.setOnClickListener {
+        play(currPlayer, binding.tv22, 2, 2)
+    }
+    binding.tv23.setOnClickListener {
+        play(currPlayer, binding.tv23, 2, 3)
+    }
+    binding.tv31.setOnClickListener {
+        play(currPlayer, binding.tv31, 3, 1)
+    }
+    binding.tv32.setOnClickListener {
+        play(currPlayer, binding.tv32, 3, 2)
+    }
+    binding.tv33.setOnClickListener {
+        play(currPlayer, binding.tv33, 3, 3)
+    }
+}
+    private fun play(status: Boolean, view: View, x: Int, y: Int) {
+        val textView: TextView = view as TextView
+        if (textView.text.toString().isEmpty() && status) {
+            textView.text = "O"
+            array[x - 1][y - 1] = 0
+            currPlayer = !status
+        } else if (textView.text.toString().isEmpty()) {
+            textView.text = "X"
+            array[x - 1][y - 1] = 1
+            currPlayer = !status
         }
 
+        if (isWin()) {
+
+            Thread {
+                    runOnUiThread {}
+                    Thread.sleep(1000)
+                reSet()
+            }.start()
+            //Toast.makeText(applicationContext,"win",Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun isWin(): Boolean {
+        return if (array[0][0] == array[0][1] && array[0][0] == array[0][2] && array[0][0] != 2)
+            true
+        else if (array[1][0] == array[1][1] && array[1][0] == array[1][2] && array[1][0] != 2)
+            true
+        else if (array[2][0] == array[2][1] && array[2][0] == array[2][2] && array[2][0] != 2)
+            true
+        else if (array[0][0] == array[1][0] && array[0][0] == array[2][0] && array[0][0] != 2)
+            true
+        else if (array[0][1] == array[1][1] && array[0][1] == array[2][1] && array[0][1] != 2)
+            true
+        else if (array[0][2] == array[1][2] && array[0][2] == array[2][2] && array[0][2] != 2)
+            true
+        else if (array[0][0] == array[1][1] && array[0][0] == array[2][2] && array[0][0] != 2)
+            true
+        else array[0][2] == array[1][1] && array[0][2] == array[2][0] && array[0][2] != 2
 
     }
-   private fun play(status: Boolean, view: View, x: Int, y: Int){
-          val textView:TextView= view as TextView
-            if(textView.text.toString() == "T" && status){
-                textView.text="O"
-                array[x-1][y-1]=0
-                currPlayer=!status
-            }
 
-            else if(textView.text.toString() == "T" ){
-               textView.text="X"
-                array[x-1][y-1]=1
-                currPlayer=!status
-            }
-
+    private fun reSet() {
+        array = Array(3) { IntArray(3) { 2 } }
+        binding.tv11.text = ""
+        binding.tv12.text = ""
+        binding.tv13.text = ""
+        binding.tv21.text = ""
+        binding.tv22.text = ""
+        binding.tv23.text = ""
+        binding.tv31.text = ""
+        binding.tv32.text = ""
+        binding.tv33.text = ""
     }
 
 }
